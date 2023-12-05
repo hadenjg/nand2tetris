e4f50ebb623be4a567528cb4de70ca9e16fc6c31
hjgeither <hjgeither@gmail.com>
2023-12-05 13:53:49 -0500

    main
    assembly for multiplying RAM[0] and RAM[1] and putting the result in RAM[2]

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,4 +7,44 @@
 // Assumes that R0 >= 0, R1 >= 0, and R0 * R1 < 32768.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-//// Replace this comment with your code.
+// in c
+// int ram0 = 3;
+// int ram1 = 4;
+// int ram2 = 0;
+// int i = 0;
+// While ( i < ram1)
+// {
+//   ram2= ram0 + ram2
+//	 i++;
+// }
+
+@i 		// i refers to some mem location
+M=1
+@R2		//refers to RAM[2]
+M=0
+
+(LOOP)
+	@i
+	D=M		// d = i value
+	
+	@R1
+	D=D-M 	// d = i-RAM[1]
+	
+	@END
+	D;JGT 	// if i-RAM[1] > 0 go to END
+	
+	@R0
+	D=M 	// set d = RAM[0]
+	
+	@R2
+	M=D+M	// set RAM[2] += RAM[0]
+	
+	@i
+	M=M+1 	// increment i
+	
+	@LOOP
+	0;JMP
+	
+(END)
+	@END
+	0;JMP	//infinite loop
