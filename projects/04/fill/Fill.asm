e446f7a1ce14e7ea4476f1acf39f7adf37591bfc
hjgeither <hjgeither@gmail.com>
2023-12-07 18:51:31 -0500

    main
    this passes the test and works correctly

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,62 @@
 // 'white' in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-//// Replace this comment with your code.
+// loop in keyboard address location
+// if > 0 jump to the loop that writes all values as black
+// if == 0 jump to the loop that writes all the values as white
+// but you don't want it to constantly be writing the whole time....
+// need 2 seperate loops
+// loop keyboard address location waiting for > 0
+// once that occurs, jump to write black screen
+// then once the screen is black jump to loop waitng for keyboard address to be ==0
+// then once kbd is == 0.  jump to loop that writes screen to be white.
+// then go back to the top and wait for keyboard to be > 0
+(START)
+	@SCREEN
+	D=A
+	@fff
+	M=D
+(BLACK)
+	@KBD
+	D=M
+	@FILL
+	D;JGT  // if greater than 0 jump to fill
+	@fff
+	D=M
+	@KBD
+	D=A-D //subtract address of kdb from address of incrementing screen
+	@BLACK
+	D;JEQ // if the address is the same as kdb go back to black	
+	@fff
+	A=M
+	M=0  //setting the value of that resides at the address of the screen to 0
+	@fff
+	M=M+1 //increment to next address
+	@BLACK
+	0;JMP
+	
+(FILL)
+	@SCREEN
+	D=A
+	@fff
+	M=D   // fff now stores the address of screen
+(WRITE)
+	@KBD
+	D=M
+	@START
+	D;JEQ //jump to the start if keyboard == 0
+	@fff
+	D=M
+	@KBD
+	D=A-D
+	@WRITE
+	D;JEQ
+	@fff
+	A=M
+	M=-1
+	@fff
+	M=M+1
+	@WRITE
+	0;JMP
+
+
